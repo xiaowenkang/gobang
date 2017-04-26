005e09979bce11db90479197e8b7d55e3eda17ba
xiaowenkang <xiaowenkang15@gamil.com>
2017-04-26 23:08:54 +0800

    gobang first commit

---
+++ src/com/edu/fivechess/BoardPanel.java
@@ -0,0 +1,11 @@
+package com.edu.fivechess;
+
+import javax.swing.JPanel;
+
+public class BoardPanel extends JPanel {
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 600;
+	public static final int MARGIN = 20;
+	// private FiveInARowModel board;
+
+}
---
+++ src/com/edu/fivechess/Point.java
@@ -0,0 +1,41 @@
+package com.edu.fivechess;
+
+import java.awt.Color;
+
+public class Point {
+	private int x;
+	private int y;
+	private Color color;
+	public static final int DIAMETER = 30;
+
+	public Point(int x, int y, Color color) {
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+}
